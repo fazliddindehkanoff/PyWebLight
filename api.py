1a59cfc2284c35f3ee656eb4463f1ec607d69ce9
Fazliddin <fazliddindehkanoff@yandex.com>
2025-07-07 14:39:45 +0500

    feat(add support for class-based views)

--- api.py
+++ api.py
@@ -1,3 +1,5 @@
+import inspect
+
 from parse import parse
 from webob import Request, Response
 
@@ -14,6 +16,8 @@ class API:
         return response(environ, start_response)
 
     def route(self, path):
+        assert path not in self.routes, "Such route already exists."
+
         def wrapper(handler):
             self.routes[path] = handler
             return handler
@@ -34,6 +38,11 @@ class API:
         handler, kwargs = self.find_handler(request_path=request.path)
 
         if handler is not None:
+            if inspect.isclass(handler):
+                handler = getattr(handler(), request.method.lower(), None)
+                if handler is None:
+                    raise AttributeError("Method not allowed", request.method)
+
             handler(request, response, **kwargs)
         else:
             self.default_response(response)
